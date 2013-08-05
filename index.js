2dcc39486baf0fd42997e8555a456218ed868b3a
James Wyse <james@jameswyse.net>
2013-08-05 19:41:25 +1000

    Tidy

--- index.js
+++ index.js
@@ -11,53 +11,56 @@ var fs            = require('fs');
 var resolve       = require('resolveit');
 var EventEmitter2 = require('eventemitter2').EventEmitter2;
 
-//
-// Export the main application
-// Inherits from EventEmitter2
-//
-var app = module.exports = new EventEmitter2({
+/**
+ * Export the main application
+ *
+ * @type {EventEmitter2}
+ */
+var App = module.exports = new EventEmitter2({
   wildcard: true,
   delimiter: '.',
   newListener: false,
   maxListeners: 0
 });
 
-//
-// Determine the correct working directory
-//
+/**
+ * Determines the correct working directory
+ */
 var rel = path.relative(process.cwd(), path.dirname(process.mainModule.filename));
 if(rel) process.chdir(rel);
 app.root = process.cwd();
 
-//
-// Load package.json
-//
+/**
+ * Loads package.json
+ *
+ * @type {Object}
+ */
 try { app.pkg = require(path.resolve(app.root, 'package.json')); }
 catch(e) {
   app.pkg = require(path.resolve(__dirname, 'package.json'));
 }
 
-//
-// Determine Environment
-//
+/**
+ * Determines the current environment
+ *
+ * @type {String}
+ */
 app.env = process.env.NODE_ENV ? process.env.NODE_ENV.toLowerCase() : 'development';
 
-//
-// Temp Data
-//
+/**
+ * Store Temp Data
+ *
+ * @type {Object}
+ */
 app.__ = {
   plugins: {}
 };
 
-//
 // Load system plugins
-//
 require('nails-config');
 require('nails-logger');
 
-//
 // Display Welcome Message
-//
 app.log.info('');
 app.log.info('Nails Framework v0.0.1');
 app.log.info('');
